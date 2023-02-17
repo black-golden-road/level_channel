0a083debcfca95314d4f191140be785829132c59
dujianjie <dujianjie@cvte.com>
2023-02-17 18:33:21 +0800

    [feature][android] update

---
+++ level_channel/android/src/main/kotlin/com/flutter/seewo/level_channel/LevelChannelManager.kt
@@ -0,0 +1,155 @@
+package com.flutter.seewo.level_channel
+
+import io.flutter.plugin.common.BasicMessageChannel
+import io.flutter.plugin.common.BinaryMessenger
+import io.flutter.plugin.common.JSONMessageCodec
+import org.json.JSONObject
+import java.util.*
+import java.util.concurrent.Executors
+import kotlin.collections.HashMap
+
+interface LevelChannelInterface {
+    fun get(path: String, parameters: JSONObject? = null, callback: (JSONObject?) -> Unit)
+
+    fun post(path: String, parameters: JSONObject? = null )
+
+    fun addGetObserver(path: String, callback:(JSONObject?,  (JSONObject) -> Unit) -> Unit)
+
+    fun removeGetObserver(path: String)
+
+    fun addPostObserver(path: String, callback: (JSONObject?) -> Unit)
+
+    fun removePostObserver(path: String)
+
+    fun deInit()
+}
+
+class LevelChannelManager(binaryMessenger: BinaryMessenger) : LevelChannelInterface {
+
+    class GetObserver(path: String, callback: (JSONObject?, ((JSONObject)->Unit))->Unit) {
+        val mPath: String = path
+        val mCallback: ((JSONObject?, ((JSONObject)->Unit))->Unit) = callback
+    }
+
+    class PostObserver(path: String, callback: (JSONObject?) -> Unit) {
+        val mPath: String = path
+        val mCallback: ((JSONObject?) -> Unit) = callback
+    }
+
+    class Response(path: String, identify: String, callback: (JSONObject?) -> Unit) {
+        val mPath: String = path
+        val mIdentify: String = identify
+        val mCallback: ((JSONObject?) -> Unit) = callback
+    }
+
+    private val writerChannel = BasicMessageChannel(binaryMessenger,
+        "com.flutter.seewo.plugins/level_channel/reader",
+        JSONMessageCodec.INSTANCE,
+        binaryMessenger.makeBackgroundTaskQueue())
+
+    private val readerChannel = BasicMessageChannel(binaryMessenger,
+        "com.flutter.seewo.plugins/level_channel/writer",
+        JSONMessageCodec.INSTANCE,
+        binaryMessenger.makeBackgroundTaskQueue())
+
+    private val responsesMap: HashMap<String, Response> = HashMap()
+    private val getObserversMap: HashMap<String, GetObserver> = HashMap()
+    private val postObserversMap: HashMap<String, PostObserver> = HashMap()
+
+    private val dispatcher = Executors.newFixedThreadPool(3)
+
+    init {
+        readerChannel.setMessageHandler { message, reply ->
+            try {
+                if (message != null && message is JSONObject) {
+                    dispatcher.submit {
+                        didReadData(message)
+                    }
+                }
+                reply.reply(null)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun didReadData(message: JSONObject) {
+        val method = message.optString("method")
+        val path = message.optString("path")
+        val identify = message.optString("id")
+        when (method) {
+            "request" -> {
+                val observer = getObserversMap[path]
+                observer?.mCallback?.let {
+                    it(message.optJSONObject("data")) { re ->
+                        send(path, "response", identify, re)
+                    }
+                }
+            }
+            "response" -> {
+                val key = "${path}request${identify}"
+                val response = responsesMap.remove(key)
+                response?.mCallback?.invoke(message.optJSONObject("data"))
+
+            }
+            "observer" -> {
+                val observer = postObserversMap[path]
+                observer?.mCallback?.invoke(message.optJSONObject("data"))
+            }
+            else -> {
+
+            }
+        }
+    }
+
+    private fun send(path: String, method: String, identify: String? = null, parameters: JSONObject? = null) {
+        val request = JSONObject()
+        request.put("path", path)
+        request.put("method", method)
+        if (parameters != null) {
+            request.put("data", parameters)
+        }
+        if (identify != null) {
+            request.put("id", identify)
+        }
+        writerChannel.send(request)
+    }
+
+    override fun get(path: String, parameters: JSONObject?, callback: (JSONObject?) -> Unit) {
+        val identify = UUID.randomUUID().toString()
+        val key =  "${path}request${identify}"
+        val response = Response(path, identify, callback)
+        responsesMap[key] = response
+        send(path, "request", identify, parameters)
+    }
+
+    override fun post(path: String, parameters: JSONObject?) {
+        send(path, "observer", parameters = parameters)
+    }
+
+    override fun addGetObserver(path: String, callback:(JSONObject?,  (JSONObject) -> Unit) -> Unit) {
+        val observer = GetObserver(path, callback)
+        getObserversMap[path] = observer
+    }
+
+    override fun removeGetObserver(path: String) {
+        getObserversMap.remove(path)
+    }
+
+    override fun addPostObserver(path: String, callback: (JSONObject?) -> Unit) {
+        val observer =  PostObserver(path, callback)
+        postObserversMap[path] = observer
+    }
+
+    override fun removePostObserver(path: String) {
+        postObserversMap.remove(path)
+    }
+
+    override fun deInit() {
+        getObserversMap.clear()
+        postObserversMap.clear()
+        responsesMap.clear()
+        readerChannel.setMessageHandler(null)
+    }
+}
+
--- level_channel/example/android/app/src/main/kotlin/com/flutter/seewo/level_channel_example/MainActivity.kt
+++ level_channel/example/android/app/src/main/kotlin/com/flutter/seewo/level_channel_example/MainActivity.kt
@@ -1,6 +1,22 @@
 package com.flutter.seewo.level_channel_example
 
+import android.os.Bundle
+import android.os.PersistableBundle
+import com.flutter.seewo.level_channel.LevelChannelInterface
+import com.flutter.seewo.level_channel.LevelChannelPlugin
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 
 class MainActivity: FlutterActivity() {
+    private var channel: LevelChannelInterface? = null
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        var m = flutterEngine?.plugins?.get(LevelChannelPlugin::class.java)
+        if (m is LevelChannelPlugin) {
+            channel = m.channel
+        }
+        channel?.addPostObserver("test") { re ->
+            println(re.toString())
+        }
+    }
 }
